9dd340f29bbe465f1c6b6caebd9e30616273e65d
Kody <dudfhr9530@gmail.com>
2017-02-14 04:42:05 +0000

    게시글과 댓글의 작성자 정보 추가하기

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,12 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-categories = ["한식","일식","중식","양식"]
+u = User.new
+u.username = "dudfhr"
+u.password = "dudfhr1"
+u.save
 
-categories.each do |category|
-  0.upto(1) do |i|
-    p = Post.new
-    p.user_id = i + 1
-    p.category = category
-    p.title = "#{category}집#{i} 이름"
-    p.content = "#{category}집#{i} 설명"
-    p.save
-  end
-end
+u = User.new
+u.username = "dudckd"
+u.password = "dudckd1"
+u.save
