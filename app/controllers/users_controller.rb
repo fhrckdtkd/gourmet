c52563ae1458e4311d9071a82cb027f7bfbf0e94
Kody <dudfhr9530@gmail.com>
2017-02-14 03:38:36 +0000

    로그인 상태 확인 기능 및 로그아웃 기능 추가하기

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,4 +40,18 @@ class UsersController < ApplicationController
 
   def logout_complete
   end
+	
+	def login_check
+    @current_user = nil
+    if !session[:user_id].nil?
+      @current_user = User.find(session[:user_id])
+    else
+      redirect_to "/users/login"
+    end
+  end
+	def logout_complete
+    reset_session
+    flash[:alert] = "성공적으로 로그아웃하였습니다."
+    redirect_to "/"
+  end
 end
